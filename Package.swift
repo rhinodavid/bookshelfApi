10c142bf7e5dcb4f2c82c883146626f615c0b4bf
David Walsh <dawalsh@gmail.com>
2016-12-19 02:10:01 -0800

    Add Kitura-CouchDB

--- Package.swift
+++ Package.swift
@@ -5,5 +5,6 @@ let package = Package(
     dependencies: [
             .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
             .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 0),
         ]
 )
