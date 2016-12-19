0971f4146e40f4e294a7670a60e4585d88a21ada
David Walsh <dawalsh@gmail.com>
2016-12-18 23:56:43 -0800

    Update packages

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "bookshelfApi",
     dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 25),
-            .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 14),
+            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+            .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
         ]
 )
