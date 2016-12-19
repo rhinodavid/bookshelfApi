ace10ddbe676b42d4075f5c24f2d99e226b68d9a
David Walsh <dawalsh@gmail.com>
2016-12-19 02:11:09 -0800

    Move file and add Couch DB

--- Sources/BookshelfAPI/main.swift
+++ Sources/BookshelfAPI/main.swift
@@ -1,3 +1,4 @@
+import CouchDB
 import HeliumLogger
 import Foundation
 import Kitura
@@ -10,12 +11,29 @@ setbuf(stdout, nil)
 // attach logger
 Log.logger = HeliumLogger()
 
+let connectionProperties = ConnectionProperties(
+  host: "localhost",
+  port: 5984,
+  secured: false,
+  username: "root",
+  password: "{password}"
+)
+
+let databaseName = "bookshelf_db"
+
+let client = CouchDBClient(connectionProperties: connectionProperties)
+let database = client.database(databaseName)
+let booksMapper = BooksMapper(withDatabase: database)
+
 // setup routes
 let router = Router()
 router.get("/") { _, response, next in 
   response.status(.OK).send(json: JSON(["hello": "world"]))
   next()
 }
+
+router.get("/books", handler: listBooksHandler)
+
 
 // start server
 Log.info("Starting server")
